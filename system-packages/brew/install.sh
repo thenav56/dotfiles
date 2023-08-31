efa8a3146299d4bb1d413344237a34b784b6dec7
Navin Ayer <ayernavin@gmail.com>
2023-08-31 15:51:51 +0545

    Add tmux
    - Add journal alias
    - Update powerlevel to ignore stdin
    - Add tmux configuration with some plugins
    - Add git-auto from logseq

--- system-packages/brew/install.sh
+++ system-packages/brew/install.sh
@@ -9,6 +9,6 @@ fi
 
 BREW_CONFIG_LOC="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+brew tap homebrew/cask-fonts
 brew install --formula `cat $BREW_CONFIG_LOC/formulas.txt`
-
 brew install --cask `cat $BREW_CONFIG_LOC/casks.txt`
