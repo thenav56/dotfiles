dcd782067245364c5bdbe5179870c065c5d7a90c
thenav56 <ayernavin@gmail.com>
2024-01-23 23:13:21 +0545

    Changes
    - Add basic tig config
    - Add lxpolkit for GUI auth
    - Add env configuration for _vim-gnupg

--- config/neovim/lua/plugin-configs/_vim-gnupg.lua
+++ config/neovim/lua/plugin-configs/_vim-gnupg.lua
@@ -1,7 +1,16 @@
--- REPLACE: Please replace this with your own gpg receipient emails
--- vim.g.GPGDefaultRecipients = {'example@example.com'}
+-- Define VimGPGDefaultRecipients in your zshrc
+
+-- Example: example@gmail.com
+local default_recipients_raw = os.getenv('VimGPGDefaultRecipients') or ''
+
+local default_recipients = {}
+for str in string.gmatch(default_recipients_raw, '([^'..','..']+)') do
+    table.insert(default_recipients, str)
+end
+
+vim.g.GPGDefaultRecipients = default_recipients
 
 vim.api.nvim_create_autocmd({'BufRead', 'BufNewFile'}, {
-    pattern = {"*.gpg"},
-    command = "User GnuPG setl textwidth=72",
+    pattern = {'*.gpg'},
+    command = 'User GnuPG setl textwidth=72',
 })
