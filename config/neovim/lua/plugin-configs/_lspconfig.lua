42d82e0a2ba78517a26e86f9d1d043d10c87923a
thenav56 <ayernavin@gmail.com>
2025-03-29 20:36:17 +0545

    Fix vim diagnostic signs

--- config/neovim/lua/plugin-configs/_lspconfig.lua
+++ config/neovim/lua/plugin-configs/_lspconfig.lua
@@ -91,11 +91,16 @@ cmp.setup.cmdline(':', {
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- Diagnostic signs
-local signs = { Error = "⛔", Warn = "⚠️", Hint = "💡", Info = "ℹ️ " }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
+vim.diagnostic.config({
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = '⛔',
+            [vim.diagnostic.severity.WARN] = '⚠️',
+            [vim.diagnostic.severity.HINT] = '💡',
+            [vim.diagnostic.severity.INFO] = 'i',
+        }
+    }
+})
 
 local function get_python_path(workspace)
   -- Use activated virtualenv.
