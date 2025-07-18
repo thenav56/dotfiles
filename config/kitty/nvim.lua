1d3e59e1c9e3b94d3e476b8009284ac7df02c603
thenav56 <ayernavin@gmail.com>
2025-07-18 10:54:47 +0545

    revert: use scrollback_pager (remove kitty_scrollback_nvim plugin)

---
+++ config/kitty/nvim.lua
@@ -0,0 +1,22 @@
+-- Set options
+vim.opt.encoding = 'utf-8'
+vim.opt.clipboard:append('unnamedplus')
+vim.opt.foldcolumn = '0'
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.laststatus = 0
+vim.opt.compatible = false
+vim.opt.list = false
+vim.opt.number = false
+vim.opt.relativenumber = false
+vim.opt.ruler = false
+vim.opt.showcmd = false
+vim.opt.showmode = false
+vim.opt.scrollback = 100000
+vim.opt.shell = 'bash'
+vim.opt.showtabline = 0
+vim.opt.termguicolors = true
+
+-- Key mappings
+vim.keymap.set('n', 'q', '<Cmd>quitall!<CR>', { noremap = true })
+vim.keymap.set('n', 'i', '<Cmd>quitall!<CR>', { noremap = true })
--- config/neovim/lua/plugins.lua
+++ config/neovim/lua/plugins.lua
@@ -14,18 +14,6 @@ return {
     },
     {'taybart/b64.nvim'},
     {'lambdalisue/suda.vim'},
-    {
-        'mikesmithgh/kitty-scrollback.nvim',
-        enabled = true,
-        lazy = true,
-        cmd = { 'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth', 'KittyScrollbackGenerateCommandLineEditing' },
-        event = { 'User KittyScrollbackLaunch' },
-        -- version = '*', -- latest stable version, may have breaking changes if major version changed
-        -- version = '^6.0.0', -- pin major version, include fixes and features that do not have breaking changes
-        config = function()
-            require('kitty-scrollback').setup()
-        end,
-    },
     {'romainl/vim-qf'},
     {
         'norcalli/nvim-colorizer.lua',
