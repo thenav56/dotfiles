186d48cc31f0b4d3c66527afa65483fe724cf6c9
thenav56 <ayernavin@gmail.com>
2025-07-21 23:24:18 +0545

    chore(nvim): fix encoding issue with LSP

--- config/neovim/lua/plugin-configs/_lspconfig.lua
+++ config/neovim/lua/plugin-configs/_lspconfig.lua
@@ -90,6 +90,10 @@ cmp.setup.cmdline(':', {
 
 -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
+capabilities.offsetEncoding = { 'utf-16' }
+capabilities.general = {
+    positionEncodings = { 'utf-16' },
+}
 
 -- Diagnostic signs
 vim.diagnostic.config({
