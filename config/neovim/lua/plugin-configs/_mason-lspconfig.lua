90fcebbdf9bfa4a0f3ad222073a86ef16e991a61
thenav56 <ayernavin@gmail.com>
2026-04-08 18:47:40 +0545

    feat: select enable for lsp with mason-lspconfig

--- config/neovim/lua/plugin-configs/_mason-lspconfig.lua
+++ config/neovim/lua/plugin-configs/_mason-lspconfig.lua
@@ -1,27 +1,29 @@
+-- https://mason-registry.dev/registry/list
+local servers = {
+    "bashls",
+    "docker_compose_language_service",
+    "dockerls",
+    "eslint",
+    "graphql",
+    "html",
+    "jsonls",
+    "lua_ls",
+    "pyright",
+    "ruff",
+    "sqlls",
+    "stylelint_lsp",
+    "ts_ls",
+    "yamlls",
+    "tflint",
+    "terraformls",
+    "harper_ls",
+    "gh_actions_ls",
+    "postgres_lsp",
+    -- Not working anymore
+    -- "nginx_language_server",
+}
+
 require('mason-lspconfig').setup {
-    automatic_enable = true,
-    -- https://mason-registry.dev/registry/list
-    ensure_installed = {
-        "bashls",
-        "docker_compose_language_service",
-        "dockerls",
-        "eslint",
-        "graphql",
-        "html",
-        "jsonls",
-        "lua_ls",
-        "pyright",
-        "ruff",
-        "sqlls",
-        "stylelint_lsp",
-        "ts_ls",
-        "yamlls",
-        "tflint",
-        "terraformls",
-        "harper_ls",
-        "gh_actions_ls",
-        "postgres_lsp",
-        -- Not working anymore
-        -- "nginx_language_server",
-    },
+    automatic_enable = servers,
+    ensure_installed = servers,
 }
